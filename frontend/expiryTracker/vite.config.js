9212029cdebe36ceceae72e392c83ee5ec68f792
damnvaibhav <rohitsjadhav66@gmail.com>
2025-06-28 14:19:41 +0530

    auth

--- frontend/expiryTracker/vite.config.js
+++ frontend/expiryTracker/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         background_color: "#ffffff",
         display: "standalone",
         orientation: "portrait",
-        start_url: "/",
+        start_url: "/auth",
         icons: [
           {
             src: "icons/pwa-icon-192.png",
